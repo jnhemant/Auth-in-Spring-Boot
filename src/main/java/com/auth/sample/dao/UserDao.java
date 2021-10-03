abc7de015973db1707016532d5226aee66a4fbbb
Hemant Jain <jnhemant319@gmail.com>
2021-10-03 22:16:22 +0530

    Added findByUsername method in UserDao

--- src/main/java/com/auth/sample/dao/UserDao.java
+++ src/main/java/com/auth/sample/dao/UserDao.java
@@ -1,9 +1,10 @@
 package com.auth.sample.dao;
 
-import com.auth.sample.model.User;
+import com.auth.sample.model.UserModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDao extends CrudRepository<User, Integer> {
+public interface UserDao extends CrudRepository<UserModel, Integer> {
+    UserModel findByUsername(String username);
 }
